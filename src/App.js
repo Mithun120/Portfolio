2241560848a65f9af06951934baa8d51474cef0b
Mithun M <mithunm.20cse@kongu.edu>
2023-06-10 20:04:11 +0530

    little animation

--- src/App.js
+++ src/App.js
@@ -26,4 +26,65 @@ function App() {
   );
 }
 
+// export default App;
+// import React, { useState, useEffect } from "react";
+// import "./App.css";
+// import "react-toastify/dist/ReactToastify.css";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import Home from "./pages/Home";
+// import Projects from "./pages/Projects";
+// import Experience from "./pages/Experience";
+// import Navbar from "./components/Navbar";
+// import Footer from "./components/Footer";
+// import ProjectDisplay from "./pages/ProjectDisplay";
+// import Contact from "./pages/Contact";
+// import DarkMode from "./pages/darkmode";
+
+// function App() {
+//   const [theme, setTheme] = useState(localStorage.getItem("theme") || "");
+//   const prefersDark =
+//     window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches;
+//   const defaultDark = theme === "dark" || (theme === "" && prefersDark);
+
+//   useEffect(() => {
+//     if (defaultDark) {
+//       setDark();
+//     } else {
+//       setLight();
+//     }
+//   }, [defaultDark]);
+
+//   const setDark = () => {
+//     localStorage.setItem("theme", "dark");
+//     document.documentElement.setAttribute("data-theme", "dark");
+//     setTheme("dark");
+//   };
+
+//   const setLight = () => {
+//     localStorage.setItem("theme", "light");
+//     document.documentElement.setAttribute("data-theme", "light");
+//     setTheme("light");
+//   };
+
+//   return (
+//     <div className={`App ${defaultDark ? "dark" : "light"}`}>
+//       <Router>
+//         <DarkMode
+//           toggleTheme={defaultDark ? setLight : setDark}
+//           defaultDark={defaultDark}
+//         />
+//         <Navbar />
+//         <Routes>
+//           <Route path="/" element={<Home />} />
+//           <Route path="/projects" element={<Projects />} />
+//           <Route path="/project/:id" element={<ProjectDisplay />} />
+//           <Route path="/experience" element={<Experience />} />
+//           <Route path="/contact" element={<Contact />} />
+//         </Routes>
+//         <Footer />
+//       </Router>
+//     </div>
+//   );
+// }
+
 export default App;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,6 +5,8 @@ import "../styles/Home.css";
 import { IconButton } from '@material-ui/core';
 import Button1 from "./Button";
 import Myimg from "../assets/Mithun.jpeg"
+
+
 function Home() {
   const redirectToUrl = (url) => {
     window.location.href = url;
@@ -16,7 +18,8 @@ function Home() {
         <div className="photo-container">
           <img className="profile-photo" src={Myimg} alt="Profile" />
         </div>
-        <h2> Hi, This is Mithun</h2>
+
+        <h2 className="animate"> Hi, This is Mithun</h2>
         <div className="prompt">
           <p>Web Developer</p>
           <IconButton aria-label="linkedin" onClick={() => redirectToUrl('https://www.linkedin.com/in/mithun-m-29b49921b/')}><LinkedInIcon /></IconButton>
@@ -31,13 +34,13 @@ function Home() {
           <li className="item">
             <h2> Front-End</h2>
             <span>
-              ReactJS,Html,Css
+              ReactJS , Html , Css , Learning NextJS
             </span>
           </li>
           <li className="item">
             <h2>Back-End</h2>
             <span>
-              NodeJS, ExpressJS, MongoDB
+              NodeJS , ExpressJS , MongoDB , Learning Graph QL
             </span>
           </li>
           <li className="item">
